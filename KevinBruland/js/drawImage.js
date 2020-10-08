367e01b7d8ee575e0acd56b28b154b23cb20ce13
Kevin Bruland <benalron@yahoo.com>
2020-10-07 23:34:22 -0400

    Initial setup and project structure

---
+++ KevinBruland/js/drawImage.js
@@ -0,0 +1,23 @@
+// Goal: Take in an image element and use canvas to draw the image with various effects
+
+var drawImage = (function(){
+    function circleDraw(imageEl){
+        console.log('circle draw fired');
+    }
+
+    function createCanvas(){
+
+    }
+
+    function appendCanvas(){
+
+    }
+
+    function hideImage(){
+
+    }
+
+    return {
+        circleDraw: circleDraw
+    };
+})();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # canvas-experiments
+
+This repository will contain various demonstrations and use cases for HTML Canvas. If you would like to contribute, please fork the repository and create a new folder with your github username.
